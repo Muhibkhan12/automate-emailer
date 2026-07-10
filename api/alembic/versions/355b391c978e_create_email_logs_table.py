6ad22e66b9f6e0660dea6ee4aab839b6e0d6c3b1
MuhibKhan12 <muhibkhan2299@gmail.com>
2026-07-10 18:31:08 +0500

    email_logs

--- api/alembic/versions/355b391c978e_create_email_logs_table.py
+++ api/alembic/versions/355b391c978e_create_email_logs_table.py
@@ -19,9 +19,21 @@ depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
-    """Upgrade schema."""
-    pass
+    op.create_table('email_logs',
+        sa.Column('id',sa.Integer(),nullable=False),
+        sa.Column('campaign_recipitent_id',sa.Integer(),sa.ForeignKey(
+            'campaign_recipients.id',
+            ondelete='CASCADE',
+        )),
+        sa.Column('sender_account_id',sa.Integer(),sa.ForeignKey(
+            'sender_accounts.id',
+            ondelete='CASCADE'
+        )),
+        sa.Column('action',sa.String(200),nullable=False),
+        sa.Column('message',sa.Text(),nullable=False),
+        sa.Column('created_at',sa.String(255),nullable=False),
 
+    )
 
 def downgrade() -> None:
     """Downgrade schema."""
