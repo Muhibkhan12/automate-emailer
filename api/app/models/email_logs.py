208be3a1ac25ca067c6efac1e7de6ec0609805ed
MuhibKhan12 <muhibkhan2299@gmail.com>
2026-07-13 21:28:19 +0500

    email_logs model done

--- api/app/models/email_logs.py
+++ api/app/models/email_logs.py
@@ -1,11 +1,11 @@
-from sqlalchemy import String,Boolean,DateTime,Text,text,ForeignKey
+from sqlalchemy import String,DateTime,ForeignKey
 from sqlalchemy.orm import  Mapped, mapped_column, relationship
 from datetime import datetime,UTC
 from cors.db import Base
 
 class EmailLogs(Base):
     __tablename__ = "email_logs"
-    id: Mapped[int] = mapped_column(primary_key=True, nullable=False)
+    id: Mapped[int] = mapped_column(primary_key=True,)
     campaign_recipients_id: Mapped[int] = mapped_column(
     ForeignKey("campaign_recipients.id"),
     nullable=False
@@ -16,10 +16,10 @@ class EmailLogs(Base):
 )
     action : Mapped[str] = mapped_column(String(200),nullable=False)
     message : Mapped[str] = mapped_column(String(1000), nullable=False)
-    created_at : Mapped[datetime] = mapped_column(datetime(timezone=True),nullable=False,default=lambda: datetime.now(UTC))
-    updated_at : Mapped[datetime] = mapped_column(datetime(timezone=True),nullable=False, default=lambda: datetime.now(UTC),onupdate=lambda: datetime.now(UTC))
+    created_at : Mapped[datetime] = mapped_column(DateTime(timezone=True),nullable=False,default=lambda: datetime.now(UTC))
+    updated_at : Mapped[datetime] = mapped_column(DateTime(timezone=True),nullable=False, default=lambda: datetime.now(UTC),onupdate=lambda: datetime.now(UTC))
 
-    campaign_recipients_id : Mapped["CampaignsRecipitent"] = relationship(
+    campaign_recipients : Mapped["CampaignsRecipitent"] = relationship(
         back_populates="email_logs"
     )
     sender_account : Mapped["SenderAccount"] = relationship(
