6ca4ecfc6dcb15395051681de0ffa2af98ce27f1
MuhibKhan12 <muhibkhan2299@gmail.com>
2026-07-13 22:03:28 +0500

    done for today

--- api/app/models/campaigns_recipitents.py
+++ api/app/models/campaigns_recipitents.py
@@ -6,8 +6,8 @@ from cors.db import Base
 class CampaignsRecipients(Base):
     __tablename__ = "campaign_recipients"
     id : Mapped[int] = mapped_column(primary_key=True)
-    campaign_id : Mapped[int] = mapped_column(ForeignKey("campaigns.id",nullable=False))
-    sender_account_id : Mapped[int] = mapped_column(ForeignKey("sender_account.id",nullable=False))
+    campaign_id : Mapped[int] = mapped_column(ForeignKey("campaigns.id"),nullable=False)
+    sender_account_id : Mapped[int] = mapped_column(ForeignKey("sender_account.id"),nullable=False)
     Recipient_name : Mapped[str] = mapped_column(String(255),nullable=False)
     Recipient_email : Mapped[str] = mapped_column(String(255),unique=True)
     company : Mapped[str] = mapped_column(String(400),nullable=False)
@@ -18,12 +18,12 @@ class CampaignsRecipients(Base):
     created_at : Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False ,default = lambda : datetime.now(UTC))
     updated_at : Mapped[datetime] = mapped_column(DateTime(timezone=True),nullable=False, default = lambda: datetime.now(UTC),onupdate = lambda:datetime.now(UTC))
 
-    sender_account : Mapped["SenderAccount"] = relationship(
+    sender_account : Mapped["senderAccount"] = relationship(
         back_populates= "CampaignsRecipients"
     )
-    campaign : Mapped["campaigns"] = relationship(
-        back_populates= "Campaigns"
+    campaign : Mapped["Campaign"] = relationship(
+        back_populates= "CampaignsRecipients"
     )
     email_log : Mapped[list["EmailLogs"]] = relationship(
-        back_populates="EmailLogs"
+        back_populates="campaign_recipient"
     )
